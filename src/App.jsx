05654e84ddd0a36b2c85bb0d3035180c027eb6a6
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2024-09-01 01:34:03 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <main>
+      <section>
+        <h1>TP 2</h1>
+        <div className="container">
+          <a href="src/TP2/01/index.html">TP 2.1</a>
+          <a href="src/TP2/02/index.html">TP 2.2</a>
+          <a href="src/TP2/03/index.html">TP 2.3</a>
+          <a href="src/TP2/04/index.html">TP 2.4</a>
+          <a href="src/TP2/05/index.html">TP 2.5</a>
+          <a href="src/TP2/06/index.html">TP 2.6</a>
+          <a href="src/TP2/07/index.html">TP 2.7</a>
+          <a href="src/TP2/08/index.html">TP 2.8</a>
+          <a href="src/TP2/09/index.html">TP 2.9</a>
+          <a href="src/TP2/10/index.html">TP 2.10</a>
+          <a href="src/TP2/11/index.html">TP 2.11</a>
+          <a href="src/TP2/12/index.html">TP 2.12</a>
+          <a href="src/TP2/13/index.html">TP 2.13</a>
+          <a href="src/TP2/14/index.html">TP 2.14</a>
+          <a href="src/TP2/15/index.html">TP 2.15</a>
+          <a href="src/TP2/16/index.html">TP 2.16</a>
+        </div>
+      </section>
+    </main>
+  );
+}
